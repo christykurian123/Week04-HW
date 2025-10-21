b0099b89081ee9d07d67a99cdbc3dbba4d9b6eba
christy <232650594+christykurian123@users.noreply.github.com>
2025-10-21 09:59:47 +0200

    Function name changed

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 let count = 0;
-function plusFunction() {
+function increase() {
   if (count < 5) {
     count++;
     document.getElementById("counter").textContent = count;
   }
 }
 
-function minusFunction() {
+function decrease() {
   if (count <= 5 && count > 0) {
     count--;
     document.getElementById("counter").textContent = count;
   }
 }
 
-function resetFunction() {
+function reset() {
   console.log("count:" + count);
-  if ((count = !0)) {
     count = 0;
     document.getElementById("counter").textContent = count;
-  }
 }
